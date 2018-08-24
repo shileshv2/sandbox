49f049c45d4130b7176911f86f8d08dd1119d73a
krishnapriya <krishnapriya.nair@v2solutions.com>
2018-08-24 16:07:09 +0530

    removed all the extra spaces

--- scripts/sh2.sh
+++ scripts/sh2.sh
@@ -3,7 +3,3 @@ echo "Hello $USER"
 echo "Hey i am" $USER "and will be telling you about the current processes"
 echo "Running processes List"
 ps
-
-
-
-
--- scripts/sh3.sh
+++ scripts/sh3.sh
@@ -3,12 +3,4 @@ echo "Hey what's Your First Name?";
 read a;
 echo "welcome $a, last name";
 read b;
-echo "thanks"
-echo "$b,bubye";
-
-
-
-
-
-
-
+echo "thanks $b"
