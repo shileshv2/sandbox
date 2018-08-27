816b779f954fc8b0adec26175531c47a17e32d58
mahesh <mahesh.sankaran@v2solutions.com>
2018-08-27 12:06:03 +0530

    changes made in read and write

--- scripts/write.sh
+++ scripts/write.sh
@@ -2,4 +2,8 @@
 input="/home/mahesh/sandbox/scripts/read.txt"
 echo "enter any content"
 read a
+<<<<<<< HEAD
 echo "$a" >> "$input"
+=======
+echo "$a" > "$input"
+>>>>>>> 29d93ea... script for read and write are added
